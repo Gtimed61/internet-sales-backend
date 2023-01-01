ac9e821f4e6bebfd699518085666c03d7f87e445
Grant Thorne <gthorne61@gmail.com>
2023-01-01 16:55:58 -0600

    models set

--- models/Category.js
+++ models/Category.js
@@ -1,3 +1,4 @@
+
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
@@ -7,6 +8,17 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+      
+    category_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
   },
   {
     sequelize,
